2c07934c22c731149629003513d01921c34045a4
lana2-2karla <allanakarla1702@gmail.com>
2022-09-19 19:50:11 -0300

    :white_check_mark: test: função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "src/jobs.csv"
+    word = "javaScript"
+    count = 122
+
+    assert count_ocurrences(path, word) == count
